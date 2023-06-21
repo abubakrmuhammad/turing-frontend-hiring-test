3a0747fe156729f80b8662850001a6c8111d5661
Abubakr Muhammad <realchampiontheno1@gmail.com>
2023-06-21 20:31:34 +0500

    setup tailwind css and font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+const AvenirLTStd = localFont({
+  src: [
+    {
+      path: '../assets/font/AvenirLTStd-Book.otf',
+      weight: '400',
+      style: 'normal',
+    },
+   {
+      path: '../assets/font/AvenirLTStd-Roman.otf',
+      weight: '500',
+      style: 'normal',
+   },
+   {
+      path: '../assets/font/AvenirLTStd-Black.otf',
+      weight: '900',
+      style: 'normal',
+   }
+  ],
+  variable: '--font-avenir'
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return <main className={`${AvenirLTStd.variable } font-sans`}>
+
+    <Component {...pageProps} />
+  </main>
 }
